1ddcb00f4ccf0a4721f2247b440565ef5731bb8d
Dawid <dyrcu15@gmail.com>
2017-01-30 19:59:28 +0100

    uncomment add-comment component's template add media query in login component clean code

--- src/app/shared/service/comment.service.ts
+++ src/app/shared/service/comment.service.ts
@@ -4,22 +4,22 @@ const now = new Date();
 
 const MOCK_COMMENTS = [
   {
-    'commentatorAvatar': 'http://www.niksebastian.com/wp-content/uploads/2014/05/sample-2-sm.jpg',
+    'commentatorAvatar': 'https://ab-prod-media-assets.s3.amazonaws.com/1/profile_pictures/raghavender-mittapalli/raghavender-mittapalli-present.png',
     'date': new Date(), 'content': 'Great!',
     'commentatorName': 'Dawid', 'commentatorLastname': 'Dyrcz'
   },
   {
-    'commentatorAvatar': 'http://www.niksebastian.com/wp-content/uploads/2014/05/sample-2-sm.jpg',
+    'commentatorAvatar': 'https://ab-prod-media-assets.s3.amazonaws.com/1/profile_pictures/raghavender-mittapalli/raghavender-mittapalli-present.png',
     'date': new Date(2017, 1, 23, 12, 20, 0), 'content': 'Great!',
     'commentatorName': 'Jan', 'commentatorLastname': 'Kowalski'
   },
   {
-    'commentatorAvatar': 'http://www.niksebastian.com/wp-content/uploads/2014/05/sample-2-sm.jpg',
+    'commentatorAvatar': 'https://ab-prod-media-assets.s3.amazonaws.com/1/profile_pictures/raghavender-mittapalli/raghavender-mittapalli-present.png',
     'date': new Date(2017, 1, 23, 13, 0, 0), 'content': 'Great!',
     'commentatorName': 'Jan', 'commentatorLastname': 'Kowalski'
   },
   {
-    'commentatorAvatar': 'http://www.niksebastian.com/wp-content/uploads/2014/05/sample-2-sm.jpg',
+    'commentatorAvatar': 'https://ab-prod-media-assets.s3.amazonaws.com/1/profile_pictures/raghavender-mittapalli/raghavender-mittapalli-present.png',
     'date': new Date(2017, 1, 23, 13, 44, 0), 'content': 'Great!',
     'commentatorName': 'Jan', 'commentatorLastname': 'Kowalski'
   }
--- src/app/shared/service/post.service.ts
+++ src/app/shared/service/post.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 const MOCK_POSTS = [
     {
-        'id': 1,
-        'publisherAvatarUrl': 'http://www.niksebastian.com/wp-content/uploads/2014/05/sample-2-sm.jpg',
+        'id': 1,        
+        'publisherAvatarUrl': 'https://ab-prod-media-assets.s3.amazonaws.com/1/profile_pictures/raghavender-mittapalli/raghavender-mittapalli-present.png',
         'publisher': 'Dawid Dyrcz',
         'publishDate': '12.12.2016',
         'content': 'Hello everyone! This is the best social network page i\'ve ever seen! :D',
