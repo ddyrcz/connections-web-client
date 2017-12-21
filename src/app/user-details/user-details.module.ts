32ff351ff46e877f3795f290c26fce5ad9b390fa
Dawid <dyrcu15@gmail.com>
2017-12-21 19:14:41 +0100

    DDED load more user's posts when scroll

--- src/app/user-details/user-details.module.ts
+++ src/app/user-details/user-details.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserDetailRoutingModule } from 'app/user-details/user-detail-routing.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { SharedModule } from 'app/shared/shared.module';
 import { PostModule } from 'app/posts-table/post/post.module';
 
@@ -9,7 +10,8 @@ import { PostModule } from 'app/posts-table/post/post.module';
   imports: [
     SharedModule,
     UserDetailRoutingModule,
-    PostModule
+    PostModule,
+    InfiniteScrollModule
   ],
   declarations: [UserDetailsComponent],
   exports: [UserDetailsComponent]
