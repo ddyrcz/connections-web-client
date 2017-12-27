180e4a2b86e507aa44ed20df67794dca60fac971
Dawid <dyrcu15@gmail.com>
2017-12-27 12:37:58 +0100

    ADDED scroll to top when clicking company logo

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -8,4 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
   ngOnInit() {
   }
+
+  logoClicked() {
+    this.scrollToTop()
+  }
+
+  scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 }
